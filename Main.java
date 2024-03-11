a5386bc53a5d6806731dd51f1875f3cbab3001eb
Ludwig-Rindler <ludwigrindler93@gmail.com>
2024-03-11 15:05:33 +0100

    initialized the accounts array to be able to add accounts to a user

--- Main.java
+++ Main.java
@@ -6,16 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         User user1 = new User(12345, "Ludwig", "123");
-        Account acc1 = new Account(1, "basic", user1);
-
-        user1.changePassword();
-
         
-        
+        Account acc = new Account(1, "basic", user1);
+    
+        user1.addAccount(acc);
+        user1.addAccount(acc);
+        user1.addAccount(acc);
+
         System.out.println(user1.getAccounts());
-        
     }
-
-
-
 }
--- User.java
+++ User.java
@@ -14,6 +14,7 @@ class User {
         userId = id;
         userName = name;
         password = userPassword;
+        accounts = new ArrayList<>();
     }
 
     // getters
@@ -51,7 +52,9 @@ class User {
     }
     
     public void addAccount(Account account){
-        this.accounts.add(account);
+        if (!account.hasUser()) {
+            this.accounts.add(account);
+        }
     }
 
     
