383ccf5250cfec6e2344ea8bdf29588e8c23af3c
Ludwig-Rindler <ludwigrindler93@gmail.com>
2024-03-12 16:19:36 +0100

    added the constructor to the Transaction class

--- Transaction.java
+++ Transaction.java
@@ -4,7 +4,13 @@ public class Transaction {
     double amount;
     String transactionType;
 
-    
+    // constructor
+    public Transaction (int transactionId, int timeStamp, double amount, String transactionType) {
+        this.transactionId = transactionId;
+        this.timeStamp = timeStamp;
+        this.amount = amount;
+        this.transactionType = transactionType;
+    }
 
 
 }
