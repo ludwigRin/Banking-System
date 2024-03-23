e75f5ef30d5dbe9cec6f86e64060a8401942e893
Ludwig Rindler <ludwigrindler93@gmail.com>
2024-03-23 11:53:43 +0100

    added some stuff

--- Bank.java
+++ Bank.java
@@ -21,12 +21,12 @@ public class Bank {
         return newUser;
     }
 
-    public Account createAccount(String type, User user) {
+    public Account createAccount(String type, User activUser) {
 
         Account newAccount = new Account(allAccounts.size(), type);
 
         allAccounts.add(newAccount);
-        user.addAccount(newAccount);
+        activUser.addAccount(newAccount);
 
         return newAccount;
     }
@@ -92,6 +92,7 @@ public class Bank {
         String inputPassword = in.nextLine();
         if (bank.correctPassword(inputUsername, inputPassword)) {
             activeUser = bank.getAllUsers().get(bank.searchForUser(inputUsername));
+            System.out.println(activeUser);
             return true;
         } else {
             System.out.println("Wrong password or username!");
@@ -101,17 +102,35 @@ public class Bank {
 
     public int mainMenuSelect(Scanner in, Bank bank) {
         System.out.println("Welcome to " + bank.bankName + "!");
-                System.out.println("How can i help you?");
-                System.out.println("1. Log in");
-                System.out.println("2. Create an Account");
-                System.out.println("0. Exit");
-                while (!in.hasNextInt()) {
-                    System.out.println("Please enter an integer from 0 to 2!");
-                    in.next();
-                }
-                int choice = in.nextInt();
-                in.nextLine();
-                return choice;
+        System.out.println("How can i help you?");
+        System.out.println("1. Log in");
+        System.out.println("2. Create an Account");
+        System.out.println("0. Exit");
+        while (!in.hasNextInt()) {
+            System.out.println("Please enter an integer from 0 to 2!");
+            in.next();
+        }
+        int choice = in.nextInt();
+        in.nextLine();
+        if (choice == 1) return 2;
+        if (choice == 2) return 3;
+        if (choice == 0) return 14;
+        return 14;
+    }
+
+    public int accountCreationButton(Scanner in) {
+        System.out.println("How would you like to proceed?");
+        System.out.println("1. Select existing bank account");
+        System.out.println("0. Create a new bank account");
+        while (!in.hasNextInt()) {
+            System.out.println("Please enter an integer!");
+            in.next();
+        }
+        int choice = in.nextInt();
+        in.nextLine();
+        if (choice == 1) return 5;
+        if (choice == 0) return 6;
+        return 14;
     }
 
     public Account accountSelection(Scanner in, User activeUser, int selectedAccountIndex, Account selectedAccount) {
@@ -143,5 +162,121 @@ public class Bank {
         } else {
             System.out.println("The passwords entered didn't match!");
         }
+    }
+
+    public void accountCreation(Scanner in, User activeUser, Bank bank) {
+        System.out.println("What type of bank account would you like to create?");
+        System.out.println("1. Basic");
+        System.out.println("2. Savings");
+        while (!in.hasNextInt()) {
+            System.out.println("Please enter an integer!");
+            in.next();
+        }
+        int selection = in.nextInt();
+        String type = (selection < 2) ? "Basic" : "Savings";
+        bank.createAccount(type, activeUser);
+    }
+
+    public int accountMenu(Scanner in, User activeUser, Account selectedAccount, int selectedAccountIndex) {
+        System.out.println("Welcome " + activeUser.getName());
+        System.out.println("What would you like to do?");
+        System.out.println("1. Withdraw");
+        System.out.println("2. Deposit");
+        System.out.println("3. Transfer");
+        System.out.println("4. Check Balance");
+        System.out.println("5. Transaction History");
+        System.out.println("0. Log Out");
+        while (!in.hasNextInt()) {
+            System.out.println("Please enter an integer from 0 to 5!");
+            in.next();
+        }
+        int choice = in.nextInt();
+        in.nextLine();
+        if (choice == 1) return 8;
+        if (choice == 2) return 9;
+        if (choice == 3) return 10;
+        if (choice == 4) return 11;
+        if (choice == 5) return 12;
+        if (choice == 0) return 13;
+        return 14;
+    }
+
+    public void accountWithdraw(Scanner in, Bank bank, Account selectedAccount) {
+        System.out.print("Please enter the amount you would like to withdraw: ");
+        double amountWithdraw = in.nextDouble();
+        in.nextLine();
+        if (amountWithdraw < selectedAccount.getBalance()) {
+            Transaction withdraw = new Transaction(bank, null, -amountWithdraw, "Withdraw", selectedAccount);
+            withdraw.execute();
+            selectedAccount.addTransaction(withdraw);
+            System.out.println("You successfully withdrew " + amountWithdraw + " Euro");
+            System.out.println("Your new balance is " + selectedAccount.getBalance());
+        } else {
+            System.out.println("You dont have enough money :(");
+        }
+    }
+
+    public void accountDeposit(Scanner in, Bank bank, Account selectedAccount ) {
+        System.out.print("Please enter the amount you would like to depsoit: ");
+        double amountDeposit = in.nextDouble();
+        in.nextLine();
+        Transaction deposit = new Transaction(bank, null, amountDeposit, "Withdraw", selectedAccount);
+        deposit.execute();
+        selectedAccount.addTransaction(deposit);
+        System.out.println("You successfully deposited " + amountDeposit + " Euro");
+        System.out.println("Your new balance is " + selectedAccount.getBalance() + " Euro");
+    }
+
+    public void accountTransfer(Scanner in, Bank bank, Account selectedAccount) {
+        System.out.print("Please enter the Account ID of the recipient: ");
+        while (!in.hasNextInt()) {
+            System.out.println("Please enter the Account ID");
+            in.next();
+        }
+        int accountID = in.nextInt();
+        in.next();
+        boolean accountExists = false;
+        Account recipient = null;
+        for (Account i : this.allAccounts) {
+            if (i.getAccountNumber() == accountID) {
+                recipient = i;
+                accountExists = true;
+            }
+        }
+        if (accountExists) {
+        
+        System.out.print("Please enter the amount you would like to transfer: ");
+        double amountTransfer = in.nextDouble();
+        in.nextLine();
+        if (amountTransfer < selectedAccount.getBalance()) {
+            Transaction transfer = new Transaction(bank, null, -amountTransfer, "Withdraw", selectedAccount, recipient);
+            transfer.execute();
+            selectedAccount.addTransaction(transfer);
+            recipient.addTransaction(transfer);
+            System.out.println("You successfully transferred " + amountTransfer + " Euro");
+            System.out.println("Your new balance is " + selectedAccount.getBalance() + " Euro");
+        } else {
+            System.out.println("You dont have enough money :(");
+        }
+        } else {
+            System.out.println("Couldn't find any account with this ID");
+        }
+    }
+
+    public void accountGetBalance(Account selectedAccount) {
+        System.out.println("Your current balance is " + selectedAccount.getBalance() + " Euro");
+    }
+
+    public void accountGetHistory(Account selectedAccount) {
+        System.out.println();
+        // add
+        // stuff
+        // here
+    }
+
+    public void userLogout(User activeUser, Account selecetedAccount, int selectedAccountIndex) {
+        activeUser = null;
+        selecetedAccount = null;
+        selectedAccountIndex = 0;
     }
 }
--- Main.java
+++ Main.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class Main {
 
     private static final int STATE_INIT = 0;
-    private static final int STATE_BANK_MAIN_MENU = ;
-    private static final int STATE_BANK_LOGIN = ;
-    private static final int STATE_BANK_CREATE_USER = ;
-    private static final int STATE_USER_SHOW_ACCOUNTS = 
-    private static final int STATE_USER_ACCOUNT_SELECTION = ;
-    private static final int STATE_USER_CREATE_ACCOUNT = ;
-    private static final int STATE_ACCOUNT_MENU = ;
-    private static final int STATE_ACCOUNT_WITHDRAW = 
-    private static final int STATE_ACCOUNT_DEPOSIT =
-    private static final int STATE_ACCOUNT_TRANSFER =
-    private static final int STATE_ACCOUNT_BALANCE =
-    private static final int STATE_ACCOUNT_HISTORY =
-    private static final int STATE_ACCOUNT_LOGOUT =
-
+    private static final int STATE_BANK_MAIN_MENU = 1;
+    private static final int STATE_BANK_LOGIN = 2;
+    private static final int STATE_BANK_CREATE_USER = 3;
+    private static final int STATE_USER_ACCOUNT_CREATE_OR_SELECT = 4;
+    private static final int STATE_USER_ACCOUNT_SELECTION = 5;
+    private static final int STATE_USER_CREATE_ACCOUNT = 6;
+    private static final int STATE_ACCOUNT_MENU = 7;
+    private static final int STATE_ACCOUNT_WITHDRAW = 8; 
+    private static final int STATE_ACCOUNT_DEPOSIT = 9;
+    private static final int STATE_ACCOUNT_TRANSFER = 10;
+    private static final int STATE_ACCOUNT_BALANCE = 11;
+    private static final int STATE_ACCOUNT_HISTORY = 12;
+    private static final int STATE_ACCOUNT_LOGOUT = 13;
+    private static final int STATE_EXIT = 14;
 
     
 
@@ -29,178 +29,199 @@ public class Main {
         User activeUser = null;
         Account selectedAccount = null;
         int selectedAccountIndex = 0;
-
-        newBank.createUser("Ludwig", "123");
-        
-        switch (currentState){
-
-            case STATE_INIT:
-                currentState = STATE_BANK_MAIN_MENU; 
-                break;
-            case STATE_BANK_MAIN_MENU:
-                currentState = newBank.mainMenuSelect(in, newBank);
-                break;
-            case STATE_BANK_LOGIN: 
-                boolean loggedIn = newBank.bankLogIn(in, newBank, activeUser);
-                if (loggedIn) currentState = STATE_USER_ACCOUNT_SELECTION;
-                else ...
-
-                break;
-                return;
-            case STATE_BANK_CREATE_USER:
-                newBank.userCreation(in, newBank);
-                currentState = STATE_BANK_MAIN_MENU;
-                break;
-            case STATE_USER_ACCOUNT_SELECTION:
-                if (activeUser.getAccounts().size() >= 1) {
-                    selectedAccount = newBank.accountSelection(in, activeUser, selectedAccountIndex, selectedAccount);
-                    currentState = STATE_ACCOUNT_MENU;
-                } else {
-                    currentState = STATE_USER_CREATE_ACCOUNT;
-                }
-                break;
-            case STATE_USER_CREATE_ACCOUNT:
-                
-                break;
-            case STATE_SHOW_ACCOUNT:
-                account.printAccounts();
-                currentState = STATE_ACTION_SELECTION;
-                break;
-
-            default: throw new RuntimeException();
-
-
-        }
-
-
-        Bank newBank = new Bank("Sparkasse");
-
-        newBank.createUser("Ludwig", "123");
-
-        
-
-
-        
-
-
         boolean on = true;
-
-        int selectedAccountIndex = 0;
-        Account selectedAccount;
-
+        
         do {
+            switch (currentState){
 
-            if (activeUser == null) {
-
-                System.out.println("Welcome to " + newBank.bankName + "!");
-                System.out.println("How can i help you?");
-                System.out.println("1. Log in");
-                System.out.println("2. Create an Account");
-                System.out.println("0. Exit");
-                while (!in.hasNextInt()) {
-                    System.out.println("Please enter an integer from 0 to 2!");
-                    in.next();
-                }
-                int choice = in.nextInt();
-                in.nextLine();
-
-                switch (choice) {
-                    case 1:
-                        System.out.print("Please enter your username: ");
-                        String inputUsername = in.nextLine();
-                        System.out.print("Please enter your password: ");
-                        String inputPassword = in.nextLine();
-                        if (newBank.correctPassword(inputUsername, inputPassword)) {
-                            activeUser = newBank.getAllUsers().get(newBank.searchForUser(inputUsername));
-                        } else {
-                            System.out.println("Wrong password or username!");
-                        }
-                        break;
-                    case 2:
-                        System.out.print("Please enter a username: ");
-                        String newUsername = in.nextLine();
-                        if (newBank.searchForUser(newUsername) != -1) {
-                            System.out.println("The username already exists!");
-                            break;
-                        }
-                        System.out.print("Please enter a password: ");
-                        String newPassword = in.nextLine();
-                        System.out.print("Please enter the password again: ");
-                        String validateNewPassword = in.nextLine();
-                        if (newPassword.equals(validateNewPassword)) {
-                            newBank.createUser(newUsername, newPassword);
-                            System.out.println("Your Account has been created");
-                            break;
-                        } else {
-                            System.out.println("The passwords entered didn't match!");
-                            break;
-                        }
-                    case 3:
-                        on = false;
-                }
-
-            } else if (activeUser.getAccounts().size() > 1 && selectedAccountIndex == 0) {
-                activeUser.printAccounts();
-                System.out.print("Please select the account you want to use: ");
-                while (!in.hasNextInt()) {
-                    System.out.println("Please enter an integer!");
-                    in.next();
-                }
-                selectedAccountIndex = in.nextInt();
-                in.nextLine();
-
+                case STATE_INIT:
+                    currentState = STATE_BANK_MAIN_MENU; 
+                    break;
+                case STATE_BANK_MAIN_MENU:
+                    currentState = newBank.mainMenuSelect(in, newBank);
+                    break;
+                case STATE_BANK_LOGIN:
+                    newBank.bankLogIn(in, newBank, activeUser);
+                    currentState = STATE_USER_ACCOUNT_CREATE_OR_SELECT;
+                    break;
+                case STATE_BANK_CREATE_USER:
+                    newBank.userCreation(in, newBank);
+                    currentState = STATE_BANK_MAIN_MENU;
+                    break;
+                case STATE_USER_ACCOUNT_CREATE_OR_SELECT:
+                    currentState = newBank.accountCreationButton(in);
+                    break;
+                case STATE_USER_ACCOUNT_SELECTION:
+                    if (activeUser.getAccounts().size() >= 1) {
+                        selectedAccount = newBank.accountSelection(in, activeUser, selectedAccountIndex, selectedAccount);
+                        currentState = STATE_ACCOUNT_MENU;
+                    } else {
+                        System.out.println("You have to create a bank account before you can continue");
+                        currentState = STATE_USER_CREATE_ACCOUNT;
+                    }
+                    break;
+                case STATE_USER_CREATE_ACCOUNT:
+                    newBank.accountCreation(in, activeUser, newBank);
+                    currentState = STATE_USER_ACCOUNT_SELECTION;
+                    break;
+                case STATE_ACCOUNT_MENU:
+                    currentState = newBank.accountMenu(in, activeUser, selectedAccount, selectedAccountIndex);
+                    break;
+                case STATE_ACCOUNT_WITHDRAW:
+                    newBank.accountWithdraw(in, newBank, selectedAccount);
+                    currentState = STATE_ACCOUNT_MENU;
+                    break;
+                case STATE_ACCOUNT_DEPOSIT:
+                    newBank.accountDeposit(in, newBank, selectedAccount);
+                    currentState = STATE_ACCOUNT_MENU;
+                    break;
+                case STATE_ACCOUNT_TRANSFER:
+                    newBank.accountTransfer(in, newBank, selectedAccount);
+                    currentState = STATE_ACCOUNT_MENU;
+                    break;
+                case STATE_ACCOUNT_BALANCE:
+                    newBank.accountGetBalance(selectedAccount);
+                    currentState = STATE_ACCOUNT_MENU;
+                    break;
+                case STATE_ACCOUNT_HISTORY:
+                    newBank.accountGetHistory(selectedAccount);
+                    currentState = STATE_ACCOUNT_MENU;
+                    break;
+                case STATE_ACCOUNT_LOGOUT:
+                    newBank.userLogout(activeUser, selectedAccount, selectedAccountIndex);
+                    currentState = STATE_BANK_MAIN_MENU;
+                    break;
+                case STATE_EXIT:
+                    on = false;
+                    break;
+                default: throw new RuntimeException();
             }
+        }
+        while (on);    
+
+
+        
+
+
+        
+
+
+
+
+
+        // do {
+
+        //     if (activeUser == null) {
+
+        //         System.out.println("Welcome to " + newBank.bankName + "!");
+        //         System.out.println("How can i help you?");
+        //         System.out.println("1. Log in");
+        //         System.out.println("2. Create an Account");
+        //         System.out.println("0. Exit");
+        //         while (!in.hasNextInt()) {
+        //             System.out.println("Please enter an integer from 0 to 2!");
+        //             in.next();
+        //         }
+        //         int choice = in.nextInt();
+        //         in.nextLine();
+
+        //         switch (choice) {
+        //             case 1:
+        //                 System.out.print("Please enter your username: ");
+        //                 String inputUsername = in.nextLine();
+        //                 System.out.print("Please enter your password: ");
+        //                 String inputPassword = in.nextLine();
+        //                 if (newBank.correctPassword(inputUsername, inputPassword)) {
+        //                     activeUser = newBank.getAllUsers().get(newBank.searchForUser(inputUsername));
+        //                 } else {
+        //                     System.out.println("Wrong password or username!");
+        //                 }
+        //                 break;
+        //             case 2:
+        //                 System.out.print("Please enter a username: ");
+        //                 String newUsername = in.nextLine();
+        //                 if (newBank.searchForUser(newUsername) != -1) {
+        //                     System.out.println("The username already exists!");
+        //                     break;
+        //                 }
+        //                 System.out.print("Please enter a password: ");
+        //                 String newPassword = in.nextLine();
+        //                 System.out.print("Please enter the password again: ");
+        //                 String validateNewPassword = in.nextLine();
+        //                 if (newPassword.equals(validateNewPassword)) {
+        //                     newBank.createUser(newUsername, newPassword);
+        //                     System.out.println("Your Account has been created");
+        //                     break;
+        //                 } else {
+        //                     System.out.println("The passwords entered didn't match!");
+        //                     break;
+        //                 }
+        //             case 3:
+        //                 on = false;
+        //         }
+
+        //     } else if (activeUser.getAccounts().size() > 1 && selectedAccountIndex == 0) {
+        //         activeUser.printAccounts();
+        //         System.out.print("Please select the account you want to use: ");
+        //         while (!in.hasNextInt()) {
+        //             System.out.println("Please enter an integer!");
+        //             in.next();
+        //         }
+        //         selectedAccountIndex = in.nextInt();
+        //         in.nextLine();
+
+        //     }
             
             
             
-            else {
-                System.out.println("Welcome " + activeUser.getName());
-                System.out.println("What would you like to do?");
-                System.out.println("1. Withdraw");
-                System.out.println("2. Deposit");
-                System.out.println("3. Transfer");
-                System.out.println("4. Transaction History");
-                System.out.println("5. Check Balance");
-                System.out.println("0. Log Out");
-                while (!in.hasNextInt()) {
-                    System.out.println("Please enter an integer from 0 to 5!");
-                    in.next();
-                }
-                int accountChoice = in.nextInt();
-                in.nextLine();
+        //     else {
+        //         System.out.println("Welcome " + activeUser.getName());
+        //         System.out.println("What would you like to do?");
+        //         System.out.println("1. Withdraw");
+        //         System.out.println("2. Deposit");
+        //         System.out.println("3. Transfer");
+        //         System.out.println("4. Transaction History");
+        //         System.out.println("5. Check Balance");
+        //         System.out.println("0. Log Out");
+        //         while (!in.hasNextInt()) {
+        //             System.out.println("Please enter an integer from 0 to 5!");
+        //             in.next();
+        //         }
+        //         int accountChoice = in.nextInt();
+        //         in.nextLine();
 
-                selectedAccount = activeUser.getAccounts().get(selectedAccountIndex - 1);
+        //         selectedAccount = activeUser.getAccounts().get(selectedAccountIndex - 1);
 
-                switch (accountChoice) {
-                    case 1:
-                        System.out.print("Please enter the amount you would like to withdraw: ");
-                        double amountWithdraw = in.nextDouble();
-                        in.nextLine();
-                        if (amountWithdraw < selectedAccount.getBalance()) {
-                            Transaction withdraw = new Transaction(newBank, null, -amountWithdraw, "Withdraw", selectedAccount);
-                            withdraw.execute();
-                            System.out.println("You successfully withdrew " + amountWithdraw + " Euro");
-                            System.out.println("Your new balance is " + selectedAccount.getBalance());
-                        } else {
-                            System.out.println("You dont have enough money :(");
-                        }
+        //         switch (accountChoice) {
+        //             case 1:
+        //                 System.out.print("Please enter the amount you would like to withdraw: ");
+        //                 double amountWithdraw = in.nextDouble();
+        //                 in.nextLine();
+        //                 if (amountWithdraw < selectedAccount.getBalance()) {
+        //                     Transaction withdraw = new Transaction(newBank, null, -amountWithdraw, "Withdraw", selectedAccount);
+        //                     withdraw.execute();
+        //                     System.out.println("You successfully withdrew " + amountWithdraw + " Euro");
+        //                     System.out.println("Your new balance is " + selectedAccount.getBalance());
+        //                 } else {
+        //                     System.out.println("You dont have enough money :(");
+        //                 }
 
-                    case 2:
-                        System.out.print("Please enter the amount you would like to depsoit: ");
-                        double amountDeposit = in.nextDouble();
-                        in.nextLine();
-                        Transaction deposit = new Transaction(newBank, null, amountDeposit, "Withdraw", selectedAccount);
-                        deposit.execute();
-                        System.out.println("You successfully deposited " + amountDeposit + " Euro");
-                        System.out.println("Your new balance is " + selectedAccount.getBalance() + " Euro");
-                        break;
+        //             case 2:
+        //                 System.out.print("Please enter the amount you would like to depsoit: ");
+        //                 double amountDeposit = in.nextDouble();
+        //                 in.nextLine();
+        //                 Transaction deposit = new Transaction(newBank, null, amountDeposit, "Withdraw", selectedAccount);
+        //                 deposit.execute();
+        //                 System.out.println("You successfully deposited " + amountDeposit + " Euro");
+        //                 System.out.println("Your new balance is " + selectedAccount.getBalance() + " Euro");
+        //                 break;
 
-                    case 5:
-                        System.out.println(selectedAccount.getBalance());
+        //             case 5:
+        //                 System.out.println(selectedAccount.getBalance());
                         
-                }
-            }
-        }    
-        while (on);
+        //         }
+        //     }
+        // }    
+        // while (on);
     }
 }
